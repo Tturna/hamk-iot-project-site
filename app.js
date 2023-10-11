b3c048d4e1fc243ee388c35a3f3396d28f8422c2
Tturna <eemelijuponaho@gmail.com>
2023-10-11 13:59:34 +0300

    send + receive

--- app.js
+++ app.js
@@ -7,27 +7,53 @@ const app = express();
 // const hostname = '127.0.0.1';
 const sitePort = 3001;
 const udpSendPort = 5001;
-const udpSendIp = '192.168.112.70';
+const udpReceivePort = 5000;
+const udpSendIp = '192.168.112.15';
 
 const onMessage = Buffer.from('1');
 const offMessage = Buffer.from('0');
 const client = dgram.createSocket('udp4');
 
+let recMsg = null;
+
 function send_udp_onMessage()
 {
+  recMsg = null;
   console.log("send on");
   client.send(onMessage, udpSendPort, udpSendIp, (err) => {
       // client.close();
   });
-}
 
-function send_udp_offMessage()
-{
-  console.log("send off");
-  client.send(offMessage, udpSendPort, udpSendIp, (err) => {
-      client.close();
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`RecMsg: ${recMsg}`);
+      resolve(recMsg != null);
+    }, 2000);
   });
 }
+
+async function send_udp_offMessage()
+{
+  console.log("send off");
+  client.send(offMessage, udpSendPort, udpSendIp, (err) => { });
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`RecMsg: ${recMsg}`);
+      resolve(recMsg != null);
+    }, 2000);
+  });
+}
+
+function close_connection()
+{
+  client.close();
+}
+
+client.on('message', (msg, rinfo) => {
+  console.log(`Received: '${msg}' from ${rinfo.address}:${rinfo.port}`);
+  recMsg = msg;
+});
 
 app.use(express.static('public'));
 
@@ -40,16 +66,50 @@ app.get('/', (req, res) => {
 
 app.post('/clickon', (req, res) => {
   console.log("sending on");
-  send_udp_onMessage();
-  res.sendStatus(201);
+  successPromise = send_udp_onMessage();
+
+  successPromise.then((success) => {
+    console.log(`success: ${success}`);
+
+    if (success)
+    {
+      console.log(`Received message: ${recMsg}`);
+      res.status(200);
+      res.send("Starting...");
+    }
+    else{
+      res.sendStatus(500);
+    }
+  });
 });
 
-app.post('/clickoff', (req, res) => {
+app.post('/clickoff', async (req, res) => {
   console.log("sending off");
-  send_udp_offMessage();
+  successPromise = send_udp_offMessage();
+
+  successPromise.then((success) => {
+    console.log(`success: ${success}`);
+
+    if (success)
+    {
+      console.log(`Received message: ${recMsg}`);
+      res.status(200);
+      res.send("Stopping...");
+    }
+    else{
+      res.sendStatus(500);
+    }
+  });
+});
+
+app.post('/clickclose', (req, res) => {
+  console.log('server closing connection...');
+  close_connection();
   res.sendStatus(201);
 });
  
+client.bind(udpReceivePort);
+
 app.listen(sitePort, () => {
   console.log(`Server running at http://localhost:${sitePort}/`);
 });
--- public/client.js
+++ public/client.js
@@ -1,10 +1,17 @@
+function update_status(status)
+{
+    document.getElementById("status").innerText = status;
+}
+
 function fetch_onMessage()
 {
     console.log("clicked on");
 
     fetch('/clickon', {method: 'POST'})
-    .then(res => {
-        console.log("sent on");
+    .then(async response => {
+        let text = await response.text();
+        console.log(`Text: ${text}`);
+        update_status(text);
     })
     .catch(e => {
         console.log(e);
@@ -16,8 +23,23 @@ function fetch_offMessage()
     console.log("clicked off");
 
     fetch('/clickoff', {method: 'POST'})
+    .then(async response => {
+        let text = await response.text();
+        console.log(`Text: ${text}`);
+        update_status(text);
+    })
+    .catch(e => {
+        console.log(e);
+    });
+}
+
+function close_connection()
+{
+    console.log("closing connection");
+
+    fetch('/clickclose', {method: 'POST'})
     .then(res => {
-        console.log("sent off");
+        console.log("sent close message");
     })
     .catch(e => {
         console.log(e);
